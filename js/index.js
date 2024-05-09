7602a373589acfa5c9228319e1489a3620e1caa3
Mayara Maciel <maciel.maya@aol.com>
2024-05-09 12:26:40 +0100

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const botao = document.querySelector(".btn-plataforma");
+
+
+const elementoPlataforma = document.querySelector(".btn-plataformas");
+
+
+botao.addEventListener("click", () => {
+
+   elementoPlataformas.classList.toggle("ativo")
+    
+});
